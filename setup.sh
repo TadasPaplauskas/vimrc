b7c2fc0154d590994b2e834142d145548eb5fa45
Tadas Paplauskas <tadas@paplauskas.lt>
2019-08-10 14:55:00 +0300

    simplified setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-git clone --recurse-submodules https://github.com/tadaspaplauskas/vimrc.git . || exit
-ln -s $(pwd) ~/.vim || exit
-ln -s $(pwd)/vimrc ~/.vimrc  || exit
+mkdir ~/.vim || exit
+git clone https://github.com/tadaspaplauskas/vimrc.git ~/.vim || exit
+ln -i ~/.vim/vimrc ~/.vimrc || exit
+
